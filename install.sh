74c284945d4d8aae609517909b172d866576720c
MUHAMMAD SLAMET DZULQURNAIN <121719789+msdzulqurnain@users.noreply.github.com>
2024-08-16 01:24:50 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+echo "SCRIPT AUTO INSTALL WINDOWS by HIDESSH"
+echo
+echo "Pilih OS yang ingin anda install"
+echo "[1] Windows 2022"
+read -p "Pilih [1]: " PILIH OS
+
+case "$PILIHOS" in
+	1|"") PILIHOS="https://download1640.mediafire.com/zgnnhi071pigBirh9zWns0ltPw1zDwToquTRoygjo44oZMWx9xdKeOjS_AuPEAw3zgU_Y2i1H4JZrKLsxqdT0W8QfYVf7_hkTXd61YYfjlsOKIbjnOcEJVxjS_lrOXbkbAU3Ki-d1_T0IjuE6FxNSK4aXCxj7gtQ1Gy7ySduXDdhEdX4/dvdpug91q05iucw/windows2022.gz";;
+	*) echo "[!] Pilihan salah"; exit;;
+esac
+
+echo "[*] Password yang saya buat sudah masuk wordlist bruteforce, silahkan masukkan password yang lebih aman!"
+read -p "[?] Masukkan password untuk akun Administrator Rdp anda(minimal 12 karakter) : " PASSADMIN
+
+IP4=$(curl -4 -s icanhazip.com)
+GW=$(ip route | awk '/default/ { print $3 }')
+
+
+cat >/tmp/net.bat<<EOF
+@ECHO OFF
+cd.>%windir%\GetAdmin
+if exist %windir%\GetAdmin (del /f /q "%windir%\GetAdmin") else (
+echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\Admin.vbs"
+"%temp%\Admin.vbs"
+del /f /q "%temp%\Admin.vbs"
+exit /b 2)
+net user Administrator $PASSADMIN
+
+
+for /f "tokens=3*" %%i in ('netsh interface show interface ^|findstr /I /R "Local.* Ethernet Ins*"') do (set InterfaceName=%%j)
+netsh -c interface ip set address name="Ethernet Instance 0" source=static address=$IP4 mask=255.255.240.0 gateway=$GW
+netsh -c interface ip add dnsservers name="Ethernet Instance 0" address=8.8.8.8 index=1 validate=no
+netsh -c interface ip add dnsservers name="Ethernet Instance 0" address=8.8.4.4 index=2 validate=no
+
+cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
+del /f /q net.bat
+exit
+EOF
+
+
+cat >/tmp/dpart.bat<<EOF
+@ECHO OFF
+echo DzStore 
+echo JENDELA INI JANGAN DITUTUP
+echo SCRIPT INI AKAN MERUBAH PORT RDP MENJADI 9999, UNTUK MENYAMBUNG KE RDP GUNAKAN ALAMAT $IP4:9999
+echo KETIK YES LALU ENTER!
+
+cd.>%windir%\GetAdmin
+if exist %windir%\GetAdmin (del /f /q "%windir%\GetAdmin") else (
+echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\Admin.vbs"
+"%temp%\Admin.vbs"
+del /f /q "%temp%\Admin.vbs"
+exit /b 2)
+
+set PORT=9999
+set RULE_NAME="Open Port %PORT%"
+
+netsh advfirewall firewall show rule name=%RULE_NAME% >nul
+if not ERRORLEVEL 1 (
+    rem Rule %RULE_NAME% already exists.
+    echo Hey, you already got a out rule by that name, you cannot put another one in!
+) else (
+    echo Rule %RULE_NAME% does not exist. Creating...
+    netsh advfirewall firewall add rule name=%RULE_NAME% dir=in action=allow protocol=TCP localport=%PORT%
+)
+
+reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v PortNumber /t REG_DWORD /d 5000
+
+ECHO SELECT VOLUME=%%SystemDrive%% > "%SystemDrive%\diskpart.extend"
+ECHO EXTEND >> "%SystemDrive%\diskpart.extend"
+START /WAIT DISKPART /S "%SystemDrive%\diskpart.extend"
+
+del /f /q "%SystemDrive%\diskpart.extend"
+cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
+del /f /q dpart.bat
+timeout 50 >nul
+del /f /q ChromeSetup.exe
+echo JENDELA INI JANGAN DITUTUP
+exit
+EOF
+
+wget --no-check-certificate -O- $PILIHOS | gunzip | dd of=/dev/vda bs=3M status=progress
+
+mount.ntfs-3g /dev/vda2 /mnt
+cd "/mnt/ProgramData/Microsoft/Windows/Start Menu/Programs/"
+cd Start* || cd start*; \
+wget https://nixpoin.com/ChromeSetup.exe
+cp -f /tmp/net.bat net.bat
+cp -f /tmp/dpart.bat dpart.bat
+
+echo "reboot Rdp dulu mazzeh baru bisa pake"
